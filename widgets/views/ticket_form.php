d6094b6241470c07a3594d0b5592e9f36917e99a
Evgeniy Krokun <treschelet@fry.(none)>
2014-08-25 09:19:45 +0400

    widget tickets

--- widgets/views/ticket_form.php
+++ widgets/views/ticket_form.php
@@ -7,9 +7,10 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use kartik\widgets\DatePicker;
 ?>
-<?php $form = ActiveForm::begin(['options' => ['class' => 'ticket-search row text-left']]);?>
+<?php $form = ActiveForm::begin(['options' => ['class' => 'ticket-search']]);?>
+<div class="row text-left">
     <div class="col-xs-12"><?= $form->field($model, 'twoways', ['enableLabel' => false])->radioList($model->getWays())?><hr></div>
-    <div class="col-xs-5 text-center">
+    <div class="col-xs-5">
         <?= $form->field($model, 'origin', ['enableLabel' => false, 'enableError' => false])->textInput()?>
         <?= $form->field($model, 'depart', ['enableLabel' => false, 'enableError' => false])->widget(DatePicker::className(), [
             'language' => Yii::$app->language,
@@ -25,7 +26,7 @@ use kartik\widgets\DatePicker;
             <?= $form->field($model, 'infants')->dropDownList([0,1,2,3,4,5,6])?>
         </div>
     </div>
-    <div class="col-xs-2">
+    <div class="col-xs-2 text-center">
         <?= Html::button('<i class="fa fa-lg fa-arrows-h"></i>', ['id' => 'swap', 'class' => 'btn btn-primary'])?>
     </div>
     <div class="col-xs-5">
@@ -43,4 +44,5 @@ use kartik\widgets\DatePicker;
     <div class="col-xs-12 text-center">
         <?= Html::submitButton('<i class="fa fa-search"></i> НАЙТИ БИЛЕТЫ', ['class' => 'btn btn-lg btn-warning'])?>
     </div>
+</div>
 <?php ActiveForm::end()?>
