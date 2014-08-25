0c4e5d6267fb4f93a7b83d25947cd97d9da81d56
Evgeniy Krokun <treschelet@fry.(none)>
2014-08-25 08:58:01 +0400

    widget tickets

--- widgets/views/ticket_form.php
+++ widgets/views/ticket_form.php
@@ -3,33 +3,44 @@
  * @var $this yii\web\View
  * @var $model treschelet\aviasales\models\Aviasales
  */
+use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use kartik\widgets\DatePicker;
 ?>
-<?php $form = ActiveForm::begin();?>
-    <?= $form->field($model, 'twoways', ['enableLabel' => false])->radioList($model->getWays())?>
-    <?= $form->field($model, 'origin')->textInput()?>
-    <?= $form->field($model, 'destination')->textInput()?>
-    <?= $form->field($model, 'depart')->widget(DatePicker::className(), [
-        'language' => Yii::$app->language,
-        'type' => DatePicker::TYPE_COMPONENT_APPEND,
-        'size' => 'lg',
-        'pluginOptions' => [
-            'autoclose' => true,
-            'format' => 'dd.mm.yyyy'
-        ]
-    ])?>
-    <?= $form->field($model, 'return')->widget(DatePicker::className(), [
-        'language' => Yii::$app->language,
-        'type' => DatePicker::TYPE_COMPONENT_APPEND,
-        'size' => 'lg',
-        'pluginOptions' => [
-            'autoclose' => true,
-            'format' => 'dd.mm.yyyy'
-        ]
-    ])?>
-    <?= $form->field($model, 'adults')->dropDownList([1 => 1,2,3,4,5,6,7,8,9])?>
-    <?= $form->field($model, 'children')->dropDownList([0,1,2,3,4,5,6])?>
-    <?= $form->field($model, 'infants')->dropDownList([0,1,2,3,4,5,6])?>
-    <?= $form->field($model, 'class')->radioList($model->getClass())?>
+<?php $form = ActiveForm::begin(['class' => 'ticket-search row text-left']);?>
+    <div class="col-xs-12"><?= $form->field($model, 'twoways', ['enableLabel' => false])->radioList($model->getWays())?><hr></div>
+    <div class="col-xs-5">
+        <?= $form->field($model, 'origin')->textInput(['class' => 'inout-lg'])?>
+        <?= $form->field($model, 'depart')->widget(DatePicker::className(), [
+            'language' => Yii::$app->language,
+            'type' => DatePicker::TYPE_COMPONENT_APPEND,
+            'size' => 'lg',
+            'pluginOptions' => [
+                'autoclose' => true,
+                'format' => 'dd.mm.yyyy'
+            ]
+        ])?>
+        <div class="ages">
+            <?= $form->field($model, 'adults')->dropDownList([1 => 1,2,3,4,5,6,7,8,9], ['class' => 'inout-lg'])?>
+            <?= $form->field($model, 'children')->dropDownList([0,1,2,3,4,5,6], ['class' => 'inout-lg'])?>
+            <?= $form->field($model, 'infants')->dropDownList([0,1,2,3,4,5,6], ['class' => 'inout-lg'])?>
+        </div>
+    </div>
+    <div class="col-xs-2">
+        <?= Html::button('<i class="fa fa-lg fa-arrows-h"></i>', ['id' => 'swap', 'class' => 'btn btn-lg btn-primary'])?>
+    </div>
+    <div class="col-xs-5">
+        <?= $form->field($model, 'destination')->textInput(['class' => 'inout-lg'])?>
+        <?= $form->field($model, 'return')->widget(DatePicker::className(), [
+            'language' => Yii::$app->language,
+            'type' => DatePicker::TYPE_COMPONENT_APPEND,
+            'size' => 'lg',
+            'pluginOptions' => [
+                'autoclose' => true,
+                'format' => 'dd.mm.yyyy'
+            ]
+        ])?>
+        <?= $form->field($model, 'class')->radioList($model->getClass())?>
+    </div>
+    <?= Html::submitButton('<i class="fa fa-search"></i> НАЙТИ БИЛЕТЫ', ['class' => 'btn btn-lg btn-warning'])?>
 <?php ActiveForm::end()?>
